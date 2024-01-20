48fee4c84347869171b0dcffecfcf276e3788e19
nikoagge <nikoagge@gmail.com>
2024-01-20 15:15:01 +0200

    Initial Commit

---
+++ QRCodeSwiftUIPOC/QRCodeSwiftUIPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeSwiftUIPOCApp.swift
+//  QRCodeSwiftUIPOC
+//
+//  Created by Nikos Aggelidis on 20/1/24.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeSwiftUIPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
